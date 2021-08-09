5527d2d5ebbc1d1c70b39fff0bf22bce36ebbabc
Robert I <robertorbbit@gmail.com>
2021-08-09 16:25:23 +0500

    Решить задачу 1

--- Program.cs
+++ Program.cs
@@ -8,5 +8,54 @@ namespace Seminar3
         {
             Console.WriteLine("Hello World!");
         }
+
+        public static string IsStepCorrect(int x0, int y0, int x1, int y1, Chessman chessman)
+        {
+            if (chessman == Chessman.Ladya)
+            {
+                if ((Math.Abs(x0 - x1) > 1 && Math.Abs(y0 - y1) == 0) || (Math.Abs(x0 - x1) == 0 && Math.Abs(y0 - y1) > 1))
+                    return "Верный ход ладьей";
+                else
+                    return "Не верный ход ладьей";
+            }
+            else if (chessman == Chessman.Kon)
+            {
+                if ((Math.Abs(x0 - x1) == 1 && Math.Abs(y0 - y1) == 2) || (Math.Abs(x0 - x1) == 2 && Math.Abs(y0 - y1) == 1))
+                    return "Верный ход конем";
+                else
+                    return "Не верный ход конем";
+            }
+            else if (chessman == Chessman.Slon)
+            {
+                if ((Math.Abs(x0 - x1) == Math.Abs(y0 - y1)))
+                    return "Верный ход слоном";
+                else
+                    return "Не верный ход слоном";
+            }
+            else if (chessman == Chessman.Ferz)
+            {
+                return "Верный ход ферзем";
+            }
+            else if (chessman == Chessman.King)
+            {
+                if ((Math.Abs(x0 - x1) == 1 || Math.Abs(y0 - y1) == 1) || (Math.Abs(x0 - x1) == Math.Abs(y0 - y1) && Math.Abs(y0 - y1) == 1))
+                    return "Верный ход королем";
+                else
+                    return "Не верный ход королем";
+            }
+            else
+                return "Такой фигуры не существует";
+
+        }
+    }
+
+    public enum Chessman
+    {
+        Slon,
+        Kon,
+        Ladya,
+        Ferz,
+        King
+
     }
 }
